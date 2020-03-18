2f3476862f635d2d6c7e5884e3caa34986c4a93f
Jideofor Nwoke <njusticej@gmail.com>
2020-03-18 13:06:19 +0100

    feat: add booking service feature

--- src/main/kotlin/com/virtualprogramming/service/BookingService.kt
+++ src/main/kotlin/com/virtualprogramming/service/BookingService.kt
@@ -1,12 +1,43 @@
 package com.virtualprogramming.service
 
+import com.virtualprogramming.model.Booking
+import com.virtualprogramming.model.Performance
 import com.virtualprogramming.model.Seat
+import com.virtualprogramming.repository.BookingRepository
+import com.virtualprogramming.repository.SeatRepository
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
 
 @Service
 class BookingService {
 
-    fun isSeatFree(seat: Seat):Boolean{
-        return true
+    @Autowired
+    lateinit var bookingRepository: BookingRepository
+
+    @Autowired
+    lateinit var seatRepository: SeatRepository
+
+    fun isSeatFree(seat: Seat, performance: Performance):Boolean{
+        val matchedBooking = findBooking(seat, performance)
+        return matchedBooking == null
+    }
+
+    fun findSeat(seatNum: Int, seatRow: Char) :Seat?{
+        val allSeats = seatRepository.findAll()
+        val foundSeat = allSeats.filter { it.num == seatNum && it.row == seatRow }
+        return foundSeat.firstOrNull();
+    }
+
+    fun findBooking(seat: Seat, performance: Performance): Booking?{
+        var bookings = bookingRepository.findAll()
+        val foundBookings = bookings.filter { it.seat == seat && it.performance == performance }
+        return foundBookings.firstOrNull()
+    }
+
+    fun reserveSeat(seat: Seat, performance: Performance, customerName: String): Booking{
+        val booking = Booking(0, customerName)
+        booking.performance = performance
+        booking.seat = seat
+        return bookingRepository.save(booking)
     }
 }
