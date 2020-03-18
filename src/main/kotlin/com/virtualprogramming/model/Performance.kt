cf1b05ffabd1bde5d9902e58454198ef2356e02d
Jideofor Nwoke <njusticej@gmail.com>
2020-03-18 13:05:13 +0100

    refactor: moved mapped entities

--- src/main/kotlin/com/virtualprogramming/model/Performance.kt
+++ src/main/kotlin/com/virtualprogramming/model/Performance.kt
@@ -1,11 +1,11 @@
 package com.virtualprogramming.model
 
-import javax.persistence.Entity
-import javax.persistence.GeneratedValue
-import javax.persistence.GenerationType
-import javax.persistence.Id
+import javax.persistence.*
 
 @Entity
 data class Performance(@Id @GeneratedValue(strategy = GenerationType.AUTO)
                   val id: Long,
-                  val title: String)
+                  val title: String){
+    @OneToMany(mappedBy = "performance")
+    lateinit var bookings: List<Booking>
+}
