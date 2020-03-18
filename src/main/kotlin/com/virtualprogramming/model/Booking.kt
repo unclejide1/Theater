67f6c50a03f1d700e944c9d19e606b6fbd68d104
Jideofor Nwoke <njusticej@gmail.com>
2020-03-18 13:03:56 +0100

    feat:add booking table ro handle booking fields

---
+++ src/main/kotlin/com/virtualprogramming/model/Booking.kt
@@ -0,0 +1,13 @@
+package com.virtualprogramming.model
+
+import javax.persistence.*
+
+@Entity
+data class Booking(@Id @GeneratedValue(strategy = GenerationType.AUTO)
+                   val id: Long,
+                   val customerName: String) {
+    @ManyToOne
+    lateinit var seat: Seat
+    @ManyToOne
+    lateinit var performance: Performance
+}
